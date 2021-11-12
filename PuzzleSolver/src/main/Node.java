7e982953dacf270b56dd6aac28e41a36c5a85fed
Pancee <panseeh.wahid.99@gmail.com>
2021-11-12 09:02:26 +0200

    added getting the path from root to node to Node class

--- PuzzleSolver/src/main/Node.java
+++ PuzzleSolver/src/main/Node.java
@@ -7,19 +7,31 @@ public class Node {
     private String state;
     private Node parent;
     private List<Node> children;
+    private List<String> pathFromRoot;
     private int cost;
     private int estimatedCostToGoal; // A*
     private int totalCost; // A*
     private int depth;
 
+    // create root node
     public Node(String state) {
         setState(state);
         children = new ArrayList<>();
+        pathFromRoot = new ArrayList<>();
+        pathFromRoot.add(state);
     }
 
+    // create non-root node
     public Node(String state, Node parent) {
-        this(state);
+        setState(state);
+        children = new ArrayList<>();
         setParent(parent);
+        setPathFromRoot(parent.pathFromRoot);
+    }
+
+    public void setPathFromRoot(List<String> pathFromRoot) {
+        this.pathFromRoot = pathFromRoot;
+        this.pathFromRoot.add(state);
     }
 
     public boolean isVisited() {
@@ -44,6 +56,7 @@ public class Node {
 
     public void setParent(Node parent) {
         this.parent = parent;
+        setPathFromRoot(parent.pathFromRoot);
     }
 
     public List<Node> getChildren() {
@@ -89,4 +102,10 @@ public class Node {
     public void setDepth(int depth) {
         this.depth = depth;
     }
+
+    public List<String> getPathFromRoot() {
+        return pathFromRoot;
+    }
+
+
 }
